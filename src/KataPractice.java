01d0fc44bd1dc69dce6b74ad73f6814afab4807c
someone <someone>
2020-01-04 11:13:46 -0600

    Updated README

--- src/KataPractice.java
+++ src/KataPractice.java
@@ -1,6 +1,28 @@
 public class KataPractice {
-    public String getInput(Integer inputStartTime, Integer inputEndTime, String inputFamily) {
-        //comment
+    public String getInput(String inputStartTime, String inputEndTime, String inputFamily) {
+        if (isInteger(inputStartTime)) {
+            Integer startTimeInt = Integer.parseInt(inputStartTime);
+            if (startTimeInt < 1700) {
+                return "-1";
+            }
+        } else {
+            return "-1";
+        }
+
         return "0";
+    }
+    public static boolean isInteger(String s) {
+        boolean isValidInteger = false;
+        try
+        {
+            Integer.parseInt(s);
+            // s is a valid integer
+            isValidInteger = true;
+        }
+        catch (NumberFormatException ex)
+        {
+            // s is not an integer
+        }
+        return isValidInteger;
     }
 }
--- test/KataPracticeTest.java
+++ test/KataPracticeTest.java
@@ -1,11 +1,39 @@
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class KataPracticeTest {
+
+    // Test to check that returned string is an integer if inputs are valid
     @Test
     public void returnNumericValue() {
         KataPractice kataPractice = new KataPractice();
-        assertEquals("0", kataPractice.getInput(0,0, "A" ));
+        // assertEquals("0", kataPractice.getInput(0,0, "A" ));
+        assertTrue(KataPractice.isInteger(kataPractice.getInput("1700","2200", "A" )));
     }
+
+    // Test to check that returned string is >= 0 if inputs are valid
+    @Test
+    public void returnPositiveValue() {
+        KataPractice kataPractice = new KataPractice();
+        Integer outputValue = Integer.parseInt(kataPractice.getInput("1700","2200", "A" ));
+        assertTrue(outputValue >= 0);
+    }
+
+    // Test to check that inputStartTime is integer, >= 1700;  return -1 if invalid
+    @Test
+    public void returnErrorBadStartTime() {
+        KataPractice kataPractice = new KataPractice();
+        assertEquals("-1", kataPractice.getInput("1600", "2200", "A"));
+    }
+
+    // Test to check that inputEndTime is integer, <= 0400;  return -1 if invalid
+
+    // Test to check that inputStartTime is before inputEndTime;  return -1 if invalid
+
+    // Test to check that inputFamily == A, B, or C;  return -1 if invalid
+
+    // Check various input combinations and validate results
 }
