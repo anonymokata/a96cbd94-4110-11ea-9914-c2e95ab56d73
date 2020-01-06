8fb0ca6af6e9e1f7266efbc835b5388ecee7d787
someone <someone>
2020-01-05 20:50:24 -0600

    Added error messages;  added Main class to manually enter inputs.

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a start time (military time - ex. 5pm = 1700): ");
+        String input1 = getInput(scanner);
+        if (input1.equals("-1")) {
+            return;
+        }
+        System.out.print("Enter an end time (military time): ");
+        String input2 = getInput(scanner);
+        if (input2.equals("-1")) {
+            return;
+        }
+        System.out.print("Enter a family (A, B, or C): ");
+        String input3 = getInput(scanner);
+        if (input3.equals("-1")) {
+            return;
+        }
+
+        KataPractice kataPractice = new KataPractice();
+        System.out.println("Total Pay: $" + kataPractice.getInput(input1,input2,input3));
+    }
+
+    private static String getInput(Scanner scanner) {
+        String input = scanner.nextLine();
+        if (input.length() == 0) {
+            return "-1";
+        } else {
+            return input;
+        }
+    }
+}
